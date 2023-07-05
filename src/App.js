76fe215b9f96fcc402b089e2689e68c52bb3fa73
amirrexa <amirrezakh123@gmail.com>
2023-07-05 13:08:23 +0330

    Added Login Form

--- src/App.js
+++ src/App.js
@@ -3,8 +3,40 @@ import { Logo } from './components/logo'
 import { Dialog } from '@reach/dialog'
 import './App.css';
 
+function LoginForm({onSubmit, buttonText}) {
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    const {username, password} = event.target.elements
+
+    onSubmit({
+      username: username.value,
+      password: password.value
+    })
+  }
+
+  return(
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='username'>Username</label>
+          <input id='username' type='text' />
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input id='password' type='password' />
+        </div>
+        <div>
+          <button type='submit'>{buttonText}</button>
+        </div>
+      </form>
+  )
+}
+
 function App() {
   const [openModal, setOpenModal] = React.useState('none')
+
+  const login = (formData) => console.log(formData)
+  const register = (formData) => console.log(formData)
+
   return (
     <div>
     <Logo width='80' height='80'/>
@@ -13,14 +45,16 @@ function App() {
       <button onClick={() => setOpenModal('register')}>Register</button>
       <Dialog aria-label='Login Form' isOpen={openModal === 'login'}>
         <div>
-          <button onClick={()=>setOpenModal('none')}>Close</button>
+          <button onClick={() => setOpenModal('none')}>Close</button>
           <h3>Login</h3>
+          <LoginForm onSubmit={login} buttonText='Login'/>
         </div>
       </Dialog>
       <Dialog aria-label='Register Form' isOpen={openModal === 'register'}>
         <div>
-          <button onClick={()=>setOpenModal('none')}>Close</button>
+          <button onClick={() => setOpenModal('none')}>Close</button>
           <h3>Register</h3>
+          <LoginForm onSubmit={register} buttonText='Register'/>
         </div>
       </Dialog>
     </div>
