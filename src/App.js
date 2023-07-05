790e5e58375502bfcfc274bf0569f61a25e0c0df
amirrexa <amirrezakh123@gmail.com>
2023-07-05 12:12:18 +0330

    Setting Up The Basic View

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React from 'react'
+import { Logo } from './components/logo'
+import { Dialog } from '@reach/dialog'
 import './App.css';
 
 function App() {
+  const [openModal, setOpenModal] = React.useState('none')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Logo width='80' height='80'/>
+      <h1>Bookshelf</h1>
+      <button onClick={() => setOpenModal('login')}>Login</button>
+      <button onClick={() => setOpenModal('register')}>Register</button>
+      <Dialog aria-label='Login Form' isOpen={openModal === 'login'}>
+        <div>
+          <button onClick={()=>setOpenModal('none')}>Close</button>
+          <h3>Login</h3>
+        </div>
+      </Dialog>
+      <Dialog aria-label='Register Form' isOpen={openModal === 'register'}>
+        <div>
+          <button onClick={()=>setOpenModal('none')}>Close</button>
+          <h3>Register</h3>
+        </div>
+      </Dialog>
     </div>
   );
 }
